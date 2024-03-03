63d8229e2c93d9814d89242005a91242f41067ce
Amumug <fabriali4@gmail.com>
2024-03-03 08:53:19 +0100

    Game-hub

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { IconType } from "react-icons";
 
-const PlatformIconList = () => {
-  return (
-    <div>PlatformIconList</div>
-  )
+interface Props {
+  platforms: Platform[];
 }
 
-export default PlatformIconList
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    nintendo: SiNintendo,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color={'gray.500'}/>
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
