f2d04eb3afca038a414e9112aa69ee9e8fed9e09
Amumug <fabriali4@gmail.com>
2024-03-07 09:42:54 +0100

    Game-hub

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import NavBar from "./components/NavBar";
 import logo from "./assets/logo.webp";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  ColorModeScript,
+  Grid,
+  GridItem,
+  Show,
+} from "@chakra-ui/react";
 import ToggleColorMode from "./components/ToggleColorMode";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <GameGrid></GameGrid>;
-    </>
+    <Grid templateAreas={{ base: `"nav" "main"`, lg: `"nav nav""aside main"` }}>
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" ><GenreList/></GridItem>
+      </Show>
+      <GridItem area="main">
+        <GameGrid/>
+      </GridItem>
+    </Grid>
   );
 }
 
