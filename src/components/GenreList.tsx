e381395c84aaecca78232025eb4ad87e9a084931
Amumug <fabriali4@gmail.com>
2024-03-09 11:53:13 +0100

    Game-hub

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,21 +5,21 @@ import getCroppedImageUrl from "../services/image-url";
 const GenreList = () => {
   const { data, isLoading, error } = useGenres();
 
-  if(error) return null
+  if (error) return null;
 
-  if(isLoading) return <Spinner/>
+  if (isLoading) return <Spinner />;
 
   return (
     <List>
       {data.map((genre) => (
-        <ListItem key={genre.id} paddingY={'5px'}>
+        <ListItem key={genre.id} paddingY={"5px"}>
           <HStack>
             <Image
               boxSize="32px"
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Text fontSize='lg'>{genre.name}</Text>
+            <Text fontSize="lg">{genre.name}</Text>
           </HStack>
         </ListItem>
       ))}
